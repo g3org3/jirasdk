e33a070c5bf44ff3ca43980e8a04171ef3199001
g3org3 <7jagjag@gmail.com>
2025-11-08 21:36:01 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 _ = setup(
     name="jirasdk",
-    verison="0.0.1",
+    verison="0.1.0",
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=["requests", "tabulate"],
